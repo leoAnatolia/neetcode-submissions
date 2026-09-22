71d600f5e10db187b4bae0292c4f5c7f0dc92a0d
ÖZGÜR AZİZ KISIR <ozgur.kisir@gmail.com>
2026-09-22 09:18:18 +0300

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.java
@@ -0,0 +1,49 @@
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        
+        // 1. Adım: Diziyi küçükten büyüğe sıralıyoruz
+        Arrays.sort(nums);
+        
+        // 2. Adım: Bir elemanı sabit tutarak diğer ikisini iki işaretçi ile arayacağız
+        for (int i = 0; i < nums.length - 2; i++) {
+            // eliminate duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    // Üçlüyü sonuca ekliyoruz
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+  // Aynı değerlerin tekrar eklenmesini önlemek için sol ve 
+  //sağ işaretçileri hareket ettiriyoruz
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    // increment left pointer if sum < 0
+                    left++;
+                } else {
+                    // decrement right pointer while sum > 0
+                    right--;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
